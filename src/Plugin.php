41ddb62b0de717ae2f314d4dcf08eda85bdda676
twinh <twinhuang@qq.com>
2018-10-17 00:25:41 +0800

    feat: 实现删除标签

--- src/Plugin.php
+++ src/Plugin.php
@@ -4,9 +4,12 @@ namespace Miaoxing\WechatTag;
 
 use Miaoxing\Plugin\BasePlugin;
 use Miaoxing\UserTag\Service\UserTagModel;
+use Wei\RetTrait;
 
 class Plugin extends BasePlugin
 {
+    use RetTrait;
+
     /**
      * {@inheritdoc}
      */
@@ -17,21 +20,34 @@ class Plugin extends BasePlugin
         $api = wei()->wechatAccount->getCurrentAccount()->createApiService();
         if ($userTag->outId) {
             return $api->callAuth('cgi-bin/tags/update', [
-                'tag' => [
-                    'id' => $userTag->outId,
-                    'name' => $userTag->name,
-                ],
+                'tag' => ['id' => $userTag->outId, 'name' => $userTag->name],
             ]);
         }
-        
+
         $ret = $api->callAuth('cgi-bin/tags/create', [
-            'tag' => [
-                'name' => $userTag->name,
-            ],
+            'tag' => ['name' => $userTag->name],
         ]);
         if ($ret['code'] === 1) {
             $userTag->outId = $ret['tag']['id'];
         }
         return $ret;
+    }
+
+    public function onBeforeUserTagDestroy(UserTagModel $userTag)
+    {
+        if (!$userTag->outId) {
+            return $this->suc();
+        }
+
+        $api = wei()->wechatAccount->getCurrentAccount()->createApiService();
+        $ret = $api->callAuth('cgi-bin/tags/delete', [
+            'tag' => ['id' => $userTag->outId],
+        ]);
+        if ($ret['code'] !== 1) {
+            return $ret;
+        }
+
+        wei()->userTagsUserModel()->delete(['tag_id' => $userTag->id]);
+        return $this->suc();
     }
 }
