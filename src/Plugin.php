1e19f167831427052edd445ea50706c4f06a239f
twinh <twinhuang@qq.com>
2018-10-17 00:13:20 +0800

    feat: 添加编辑标签同步给微信

--- src/Plugin.php
+++ src/Plugin.php
@@ -3,6 +3,7 @@
 namespace Miaoxing\WechatTag;
 
 use Miaoxing\Plugin\BasePlugin;
+use Miaoxing\UserTag\Service\UserTagModel;
 
 class Plugin extends BasePlugin
 {
@@ -10,4 +11,27 @@ class Plugin extends BasePlugin
      * {@inheritdoc}
      */
     protected $name = '微信用户标签';
+
+    public function onBeforeUserTagSave(UserTagModel $userTag)
+    {
+        $api = wei()->wechatAccount->getCurrentAccount()->createApiService();
+        if ($userTag->outId) {
+            return $api->callAuth('cgi-bin/tags/update', [
+                'tag' => [
+                    'id' => $userTag->outId,
+                    'name' => $userTag->name,
+                ],
+            ]);
+        }
+        
+        $ret = $api->callAuth('cgi-bin/tags/create', [
+            'tag' => [
+                'name' => $userTag->name,
+            ],
+        ]);
+        if ($ret['code'] === 1) {
+            $userTag->outId = $ret['tag']['id'];
+        }
+        return $ret;
+    }
 }
